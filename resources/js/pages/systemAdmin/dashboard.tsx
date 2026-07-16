e418e79fa7b4758ceed830c2cd44c4a285672447
sc-miyahara <t.miyahara@secure-cycle.co.jp>
2026-07-16 12:52:14 +0900

    [feat]ダッシュボードの追加

--- resources/js/pages/systemAdmin/dashboard.tsx
+++ resources/js/pages/systemAdmin/dashboard.tsx
@@ -1,7 +1,67 @@
-export default function Dashboard() {
-    return (
-        <div>
-            <h1>System Admin Dashboard</h1>
-        </div>
-    );
+import * as React from 'react';
+import { Head } from '@inertiajs/react';
+import type {} from '@mui/x-date-pickers/themeAugmentation';
+import type {} from '@mui/x-charts/themeAugmentation';
+import type {} from '@mui/x-data-grid/themeAugmentation';
+import type {} from '@mui/x-tree-view/themeAugmentation';
+import { alpha } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import AppNavbar from '../../components/ui/AppNavbar';
+import Header from '../../components/ui/Header';
+import MainGrid from '../../components/ui/MainGrid';
+import SideMenu from '../../components/ui/SideMenu';
+import AppTheme from '../../themes/shared/AppTheme';
+import {
+  chartsCustomizations,
+  dataGridCustomizations,
+  datePickersCustomizations,
+  treeViewCustomizations,
+} from '../../themes/customizations';
+
+const xThemeComponents = {
+  ...chartsCustomizations,
+  ...dataGridCustomizations,
+  ...datePickersCustomizations,
+  ...treeViewCustomizations,
+};
+
+export default function Dashboard(props: { disableCustomTheme?: boolean }) {
+  return (
+    <>
+        <Head title="ダッシュボード" />
+        <AppTheme {...props} themeComponents={xThemeComponents}>
+        <CssBaseline enableColorScheme />
+        <Box sx={{ display: 'flex' }}>
+            <SideMenu />
+            <AppNavbar />
+            {/* Main content */}
+            <Box
+            component="main"
+            sx={(theme) => ({
+                flexGrow: 1,
+                backgroundColor: theme.vars
+                ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                : alpha(theme.palette.background.default, 1),
+                overflow: 'auto',
+            })}
+            >
+            <Stack
+                spacing={2}
+                sx={{
+                alignItems: 'center',
+                mx: 3,
+                pb: 10,
+                mt: { xs: 8, md: 0 },
+                }}
+            >
+                <Header />
+                <MainGrid />
+            </Stack>
+            </Box>
+        </Box>
+        </AppTheme>
+    </>
+  );
 }
