be3e6604a9377c857f462d9d308047c37c429c5d
sc-miyahara <t.miyahara@secure-cycle.co.jp>
2026-07-17 16:54:20 +0900

    [fix]ユーザ登録時の重複エラー

--- app/Http/Requests/Admin/UserStoreRequest.php
+++ app/Http/Requests/Admin/UserStoreRequest.php
@@ -27,7 +27,7 @@ class UserStoreRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email'],
+            'email' => ['required', 'string', 'email', 'unique:users,email'],
             'password' => ['required', 'string', 'confirmed'],
             'role' => ['required', new Enum(UserRole::class)],
         ];
